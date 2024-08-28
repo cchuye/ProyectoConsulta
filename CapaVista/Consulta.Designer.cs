b689991d615e0e9237c4af883c391b707a0a9ceb
coradoa <acoradop@miumg.edu.gt>
2024-08-27 20:52:39 -0600

    cambiando interfaz

--- CapaVista/Consulta.Designer.cs
+++ CapaVista/Consulta.Designer.cs
@@ -53,7 +53,7 @@ namespace CapaVista
             this.Dgv_consulta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.Dgv_consulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.Dgv_consulta.Location = new System.Drawing.Point(43, 354);
-            this.Dgv_consulta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Dgv_consulta.Margin = new System.Windows.Forms.Padding(4);
             this.Dgv_consulta.Name = "Dgv_consulta";
             this.Dgv_consulta.RowHeadersWidth = 51;
             this.Dgv_consulta.Size = new System.Drawing.Size(969, 185);
@@ -62,32 +62,34 @@ namespace CapaVista
             // 
             // BtnConsulta
             // 
+            this.BtnConsulta.BackColor = System.Drawing.Color.YellowGreen;
             this.BtnConsulta.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnConsulta.Location = new System.Drawing.Point(515, 86);
-            this.BtnConsulta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnConsulta.Margin = new System.Windows.Forms.Padding(4);
             this.BtnConsulta.Name = "BtnConsulta";
             this.BtnConsulta.Size = new System.Drawing.Size(133, 54);
             this.BtnConsulta.TabIndex = 1;
             this.BtnConsulta.Text = "Consulta";
-            this.BtnConsulta.UseVisualStyleBackColor = true;
+            this.BtnConsulta.UseVisualStyleBackColor = false;
             this.BtnConsulta.Click += new System.EventHandler(this.BtnConsulta_Click);
             // 
             // btn_ingresar
             // 
+            this.btn_ingresar.BackColor = System.Drawing.Color.Navy;
             this.btn_ingresar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_ingresar.Location = new System.Drawing.Point(688, 86);
-            this.btn_ingresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_ingresar.Margin = new System.Windows.Forms.Padding(4);
             this.btn_ingresar.Name = "btn_ingresar";
             this.btn_ingresar.Size = new System.Drawing.Size(133, 54);
             this.btn_ingresar.TabIndex = 2;
             this.btn_ingresar.Text = "Ingresar";
-            this.btn_ingresar.UseVisualStyleBackColor = true;
+            this.btn_ingresar.UseVisualStyleBackColor = false;
             this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click);
             // 
             // txt_codigo
             // 
             this.txt_codigo.Location = new System.Drawing.Point(173, 50);
-            this.txt_codigo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_codigo.Margin = new System.Windows.Forms.Padding(4);
             this.txt_codigo.Name = "txt_codigo";
             this.txt_codigo.Size = new System.Drawing.Size(239, 22);
             this.txt_codigo.TabIndex = 3;
@@ -95,7 +97,7 @@ namespace CapaVista
             // txt_nombre
             // 
             this.txt_nombre.Location = new System.Drawing.Point(173, 106);
-            this.txt_nombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_nombre.Margin = new System.Windows.Forms.Padding(4);
             this.txt_nombre.Name = "txt_nombre";
             this.txt_nombre.Size = new System.Drawing.Size(239, 22);
             this.txt_nombre.TabIndex = 4;
@@ -103,7 +105,7 @@ namespace CapaVista
             // txt_estado
             // 
             this.txt_estado.Location = new System.Drawing.Point(173, 268);
-            this.txt_estado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_estado.Margin = new System.Windows.Forms.Padding(4);
             this.txt_estado.Name = "txt_estado";
             this.txt_estado.Size = new System.Drawing.Size(239, 22);
             this.txt_estado.TabIndex = 5;
@@ -111,7 +113,7 @@ namespace CapaVista
             // txt_puesto
             // 
             this.txt_puesto.Location = new System.Drawing.Point(173, 153);
-            this.txt_puesto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_puesto.Margin = new System.Windows.Forms.Padding(4);
             this.txt_puesto.Name = "txt_puesto";
             this.txt_puesto.Size = new System.Drawing.Size(239, 22);
             this.txt_puesto.TabIndex = 5;
@@ -119,7 +121,7 @@ namespace CapaVista
             // txt_departamento
             // 
             this.txt_departamento.Location = new System.Drawing.Point(173, 215);
-            this.txt_departamento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_departamento.Margin = new System.Windows.Forms.Padding(4);
             this.txt_departamento.Name = "txt_departamento";
             this.txt_departamento.Size = new System.Drawing.Size(239, 22);
             this.txt_departamento.TabIndex = 6;
@@ -182,38 +184,41 @@ namespace CapaVista
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.YellowGreen;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(852, 86);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 54);
             this.button1.TabIndex = 12;
             this.button1.Text = "Refrescar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Navy;
             this.button2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(515, 161);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(133, 54);
             this.button2.TabIndex = 13;
             this.button2.Text = "Eliminar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.YellowGreen;
             this.button3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(688, 161);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(133, 54);
             this.button3.TabIndex = 14;
             this.button3.Text = "Modificar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Consulta
@@ -237,7 +242,7 @@ namespace CapaVista
             this.Controls.Add(this.btn_ingresar);
             this.Controls.Add(this.BtnConsulta);
             this.Controls.Add(this.Dgv_consulta);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Consulta";
             this.Text = "Consulta";
             ((System.ComponentModel.ISupportInitialize)(this.Dgv_consulta)).EndInit();

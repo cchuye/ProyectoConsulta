164505a01ba249dd87e4449d71b010f74b46028a
coradoa <acoradop@miumg.edu.gt>
2024-08-27 20:11:12 -0600

    Subiendo cambios de interfaz y botones
    autora: Andrea

--- CapaVista/Consulta.Designer.cs
+++ CapaVista/Consulta.Designer.cs
@@ -52,84 +52,99 @@ namespace CapaVista
             // 
             this.Dgv_consulta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.Dgv_consulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Dgv_consulta.Location = new System.Drawing.Point(32, 288);
+            this.Dgv_consulta.Location = new System.Drawing.Point(43, 354);
+            this.Dgv_consulta.Margin = new System.Windows.Forms.Padding(4);
             this.Dgv_consulta.Name = "Dgv_consulta";
-            this.Dgv_consulta.Size = new System.Drawing.Size(727, 150);
+            this.Dgv_consulta.RowHeadersWidth = 51;
+            this.Dgv_consulta.Size = new System.Drawing.Size(969, 185);
             this.Dgv_consulta.TabIndex = 0;
             this.Dgv_consulta.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dgv_consulta_CellContentClick);
             // 
             // BtnConsulta
             // 
+            this.BtnConsulta.BackColor = System.Drawing.Color.SandyBrown;
             this.BtnConsulta.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnConsulta.Location = new System.Drawing.Point(386, 70);
+            this.BtnConsulta.Location = new System.Drawing.Point(515, 86);
+            this.BtnConsulta.Margin = new System.Windows.Forms.Padding(4);
             this.BtnConsulta.Name = "BtnConsulta";
-            this.BtnConsulta.Size = new System.Drawing.Size(100, 44);
+            this.BtnConsulta.Size = new System.Drawing.Size(133, 54);
             this.BtnConsulta.TabIndex = 1;
             this.BtnConsulta.Text = "Consulta";
-            this.BtnConsulta.UseVisualStyleBackColor = true;
+            this.BtnConsulta.UseVisualStyleBackColor = false;
             this.BtnConsulta.Click += new System.EventHandler(this.BtnConsulta_Click);
             // 
             // btn_ingresar
             // 
+            this.btn_ingresar.BackColor = System.Drawing.Color.Purple;
             this.btn_ingresar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_ingresar.Location = new System.Drawing.Point(516, 70);
+            this.btn_ingresar.Location = new System.Drawing.Point(688, 86);
+            this.btn_ingresar.Margin = new System.Windows.Forms.Padding(4);
             this.btn_ingresar.Name = "btn_ingresar";
-            this.btn_ingresar.Size = new System.Drawing.Size(100, 44);
+            this.btn_ingresar.Size = new System.Drawing.Size(133, 54);
             this.btn_ingresar.TabIndex = 2;
             this.btn_ingresar.Text = "Ingresar";
-            this.btn_ingresar.UseVisualStyleBackColor = true;
+            this.btn_ingresar.UseVisualStyleBackColor = false;
             this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click);
             // 
             // txt_codigo
             // 
-            this.txt_codigo.Location = new System.Drawing.Point(130, 41);
+            this.txt_codigo.Location = new System.Drawing.Point(173, 50);
+            this.txt_codigo.Margin = new System.Windows.Forms.Padding(4);
             this.txt_codigo.Name = "txt_codigo";
-            this.txt_codigo.Size = new System.Drawing.Size(180, 20);
+            this.txt_codigo.Size = new System.Drawing.Size(239, 22);
             this.txt_codigo.TabIndex = 3;
             // 
             // txt_nombre
             // 
-            this.txt_nombre.Location = new System.Drawing.Point(130, 86);
+            this.txt_nombre.Location = new System.Drawing.Point(173, 106);
+            this.txt_nombre.Margin = new System.Windows.Forms.Padding(4);
             this.txt_nombre.Name = "txt_nombre";
-            this.txt_nombre.Size = new System.Drawing.Size(180, 20);
+            this.txt_nombre.Size = new System.Drawing.Size(239, 22);
             this.txt_nombre.TabIndex = 4;
             // 
             // txt_estado
             // 
-            this.txt_estado.Location = new System.Drawing.Point(130, 218);
+            this.txt_estado.Location = new System.Drawing.Point(173, 268);
+            this.txt_estado.Margin = new System.Windows.Forms.Padding(4);
             this.txt_estado.Name = "txt_estado";
-            this.txt_estado.Size = new System.Drawing.Size(180, 20);
+            this.txt_estado.Size = new System.Drawing.Size(239, 22);
             this.txt_estado.TabIndex = 5;
             // 
             // txt_puesto
             // 
-            this.txt_puesto.Location = new System.Drawing.Point(130, 124);
+            this.txt_puesto.Location = new System.Drawing.Point(173, 153);
+            this.txt_puesto.Margin = new System.Windows.Forms.Padding(4);
             this.txt_puesto.Name = "txt_puesto";
-            this.txt_puesto.Size = new System.Drawing.Size(180, 20);
+            this.txt_puesto.Size = new System.Drawing.Size(239, 22);
             this.txt_puesto.TabIndex = 5;
             // 
             // txt_departamento
             // 
-            this.txt_departamento.Location = new System.Drawing.Point(130, 175);
+            this.txt_departamento.Location = new System.Drawing.Point(173, 215);
+            this.txt_departamento.Margin = new System.Windows.Forms.Padding(4);
             this.txt_departamento.Name = "txt_departamento";
-            this.txt_departamento.Size = new System.Drawing.Size(180, 20);
+            this.txt_departamento.Size = new System.Drawing.Size(239, 22);
             this.txt_departamento.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 41);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.Size = new System.Drawing.Size(69, 29);
             this.label1.TabIndex = 7;
             this.label1.Text = "COD";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 86);
+            this.label2.Font = new System.Drawing.Font("Map Symbols", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.Size = new System.Drawing.Size(52, 14);
             this.label2.TabIndex = 8;
             this.label2.Text = "Nombre";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -137,68 +152,80 @@ namespace CapaVista
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 131);
+            this.label3.Font = new System.Drawing.Font("Map Symbols", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(51, 161);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.Size = new System.Drawing.Size(45, 14);
             this.label3.TabIndex = 9;
             this.label3.Text = "Puesto";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(42, 178);
+            this.label4.Font = new System.Drawing.Font("Map Symbols", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 219);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.Size = new System.Drawing.Size(45, 14);
             this.label4.TabIndex = 10;
             this.label4.Text = "Depa";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(42, 221);
+            this.label5.Font = new System.Drawing.Font("Map Symbols", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(56, 272);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(54, 14);
             this.label5.TabIndex = 11;
             this.label5.Text = "Estado";
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Pink;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(639, 70);
+            this.button1.Location = new System.Drawing.Point(852, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 44);
+            this.button1.Size = new System.Drawing.Size(133, 54);
             this.button1.TabIndex = 12;
             this.button1.Text = "Refrescar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.LightCyan;
             this.button2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(386, 131);
+            this.button2.Location = new System.Drawing.Point(515, 161);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 44);
+            this.button2.Size = new System.Drawing.Size(133, 54);
             this.button2.TabIndex = 13;
             this.button2.Text = "Eliminar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
             this.button3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(516, 131);
+            this.button3.Location = new System.Drawing.Point(688, 161);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 44);
+            this.button3.Size = new System.Drawing.Size(133, 54);
             this.button3.TabIndex = 14;
             this.button3.Text = "Modificar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Consulta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(858, 450);
+            this.ClientSize = new System.Drawing.Size(1144, 554);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -215,6 +242,7 @@ namespace CapaVista
             this.Controls.Add(this.btn_ingresar);
             this.Controls.Add(this.BtnConsulta);
             this.Controls.Add(this.Dgv_consulta);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Consulta";
             this.Text = "Consulta";
             ((System.ComponentModel.ISupportInitialize)(this.Dgv_consulta)).EndInit();
